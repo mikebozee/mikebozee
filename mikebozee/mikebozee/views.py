94b0e120fd10c725b95fd7fe9ead614a3c1f0ca9
Mike Bozee <mike@mikebozee.com>
2018-08-14 21:07:33 -0700

    articles and projects on homepage.

--- mikebozee/mikebozee/views.py
+++ mikebozee/mikebozee/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
-def index(request):
-	# return HttpResponse("Testing 123.")
-	return render(request, 'base.html', {})
+from apps.articles.models import Article
+from apps.projects.models import Project
+
+def index(request, template='base.html'):
+	context = {
+		'articles': Article.objects.all(),
+		'projects': Project.objects.all(),
+	}
+	return render(request, template, context)
