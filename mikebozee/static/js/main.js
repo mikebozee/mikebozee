61ece55eebe40dfbf20bc0df9178fc17591633b9
Mike Bozee <mike@mikebozee.com>
2018-09-27 11:21:01 -0700

    Job title text rotator added.

---
+++ mikebozee/static/js/main.js
@@ -0,0 +1,8 @@
+var jobTitles = ["Software Development Engineer", "Web Development Engineer", "Web Application Developer"];
+
+function rotateJobTitle() {
+  var jt = $("#job-title-rotate").data("jobTitle") || 0;
+  $("#job-title-rotate").data("jobTitle", jt == jobTitles.length -1 ? 0 : jt + 1).text(jobTitles[jt]).fadeIn()
+              .delay(3000).fadeOut(800, rotateJobTitle);
+}
+$(rotateJobTitle);
