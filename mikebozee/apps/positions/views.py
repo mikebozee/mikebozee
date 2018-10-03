567c09cdee8c09f305f5f22f27d25df3c50c1018
Mike Bozee <mike@mikebozee.com>
2018-10-02 21:21:57 -0700

    references included in positions views.

--- mikebozee/apps/positions/views.py
+++ mikebozee/apps/positions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Position
+from apps.references.models import Reference
 
 
 def position_list(request):
@@ -9,6 +10,9 @@ def position_list(request):
 	return render(request, 'positions/list.html', {'positions': positions})
 
 
-def position_detail(request, slug):
-	position = get_object_or_404(Position, slug=slug)
-	return render(request, 'positions/detail.html', {'position': position})
+def position_detail(request, slug, template='positions/detail.html'):
+	context = {
+		'position':		get_object_or_404(Position, slug=slug),
+		'references':	Reference.objects.filter(published=True),
+	}
+	return render(request, template, context)
